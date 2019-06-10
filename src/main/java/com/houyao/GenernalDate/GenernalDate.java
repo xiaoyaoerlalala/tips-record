9179978fe0ec9a7498a876703f40929ded04edbc
houyao <319198859@qq.com>
2019-06-10 11:38:33 +0800

    提交“获取当前日期的字符串”示例

--- src/main/java/com/houyao/GenernalDate/GenernalDate.java
+++ src/main/java/com/houyao/GenernalDate/GenernalDate.java
@@ -1,6 +1,7 @@
 package com.houyao.GenernalDate;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -9,6 +10,10 @@ import java.util.Date;
  * @Date: Created in 10:33:57 2019/5/31
  */
 public class GenernalDate {
+    public static void main(String[] args) {
+        System.out.println(getNowDate());
+    }
+
     /**
     * @Description: 获取当前日期的字符串
     * @auther: houyao
@@ -23,8 +28,20 @@ public class GenernalDate {
         String nowString=dateFormat.format(now);
         return nowString;
     }
-
-    public static void main(String[] args) {
-        System.out.println(getNowDate());
+    /**
+    * @Description: 获取指定时间的long类型毫秒数
+    * @auther: houyao
+    * @date: 11:36:16 2019/6/10
+    * @param: []
+    * @return: long
+    *
+    */
+    public static long getSpecifiedMillis(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.clear();
+        //注意此处有坑：0表示1月，1表示2月，以此类推
+        calendar.set(2018,0,1);
+        long millis = calendar.getTimeInMillis();
+        return millis;
     }
 }
